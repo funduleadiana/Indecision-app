1a31099563dc5e5e3d9a8b0a416791d417f901ce
funduleadiana <diana.fundulea@yahoo.com>
2021-05-27 14:54:18 +0100

    make decision funct

--- src/playground/indecision.js
+++ src/playground/indecision.js
@@ -24,6 +24,12 @@ const removeAll = () => {
     render();
 }
 
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+}
+
 const render = () => {
     const template = (
         <div>
@@ -31,6 +37,7 @@ const render = () => {
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? 'Here are your options' : 'No options' }</p>
             <p>{app.options.length}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
             <button onClick={removeAll}>Remove all</button>
             <ol>
                 {
