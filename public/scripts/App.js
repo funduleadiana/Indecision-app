31dbd668494b95fc7f859218e41736170e8af0b3
funduleadiana <diana.fundulea@yahoo.com>
2021-05-28 11:16:36 +0100

    counter react

--- public/scripts/App.js
+++ public/scripts/App.js
@@ -1,39 +1,83 @@
 'use strict';
 
-var root = document.getElementById('root');
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var visibility = false;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var toggleDetails = function toggleDetails() {
-    visibility = !visibility;
-    render();
-};
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-var render = function render() {
-    var visibilityToggle = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            'Visibility Toggle'
-        ),
-        React.createElement(
-            'button',
-            { onClick: toggleDetails },
-            visibility ? 'Hide details' : 'Show details'
-        ),
-        visibility && React.createElement(
-            'div',
-            null,
-            React.createElement(
-                'p',
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Counter = function (_React$Component) {
+    _inherits(Counter, _React$Component);
+
+    function Counter(props) {
+        _classCallCheck(this, Counter);
+
+        var _this = _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).call(this, props));
+
+        _this.handleAddOne = _this.handleAddOne.bind(_this);
+        _this.handleMinusOne = _this.handleMinusOne.bind(_this);
+        _this.handleResetCount = _this.handleResetCount.bind(_this);
+        _this.state = {
+            count: 0
+        };
+        return _this;
+    }
+
+    _createClass(Counter, [{
+        key: 'handleAddOne',
+        value: function handleAddOne() {
+            this.setState(function (prevState) {
+                return {
+                    count: prevState.count + 1
+                };
+            });
+        }
+    }, {
+        key: 'handleMinusOne',
+        value: function handleMinusOne() {
+            this.setState(function (prevState) {
+                return {
+                    count: prevState.count - 1
+                };
+            });
+        }
+    }, {
+        key: 'handleResetCount',
+        value: function handleResetCount() {}
+    }, {
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
                 null,
-                'These are some details ...click to hide'
-            )
-        )
-    );
-    ReactDOM.render(visibilityToggle, root);
-};
+                React.createElement(
+                    'h1',
+                    null,
+                    'Count : ',
+                    this.state.count
+                ),
+                React.createElement(
+                    'button',
+                    { onClick: this.handleAddOne },
+                    '+1'
+                ),
+                React.createElement(
+                    'button',
+                    { onClick: this.handleMinusOne },
+                    '-1'
+                ),
+                React.createElement(
+                    'button',
+                    { onClick: this.handleResetCount },
+                    'Reset counter'
+                )
+            );
+        }
+    }]);
 
-render();
+    return Counter;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Counter, null), document.getElementById('root'));
---
+++ src/playground/counter.js
@@ -0,0 +1,42 @@
+class Counter extends React.Component{
+    constructor(props){
+        super(props)
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleResetCount = this.handleResetCount.bind(this);
+        this.state = {
+            count: 0
+        }
+    }
+    handleAddOne(){
+        this.setState((prevState)=>{
+            return {
+                count: prevState.count+1
+            };
+        })
+
+    }
+    handleMinusOne(){
+        this.setState((prevState)=> {
+            return { 
+                count: prevState.count - 1
+            }
+        })
+
+    }
+    handleResetCount(){
+        
+    }
+    render(){
+        return(
+            <div>
+                <h1>Count : {this.state.count}</h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleResetCount}>Reset counter</button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
