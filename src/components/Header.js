582803d21563bcb269deb17e8b52fb5db1ec65f1
funduleadiana <diana.fundulea@yahoo.com>
2021-06-03 10:22:37 +0100

    refactoring code implicit return

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
-const Header = (props)=>{
-    return( 
+const Header = (props)=>( 
         <div>
             <h1>{props.title}</h1>
             <h2>{props.subtitle}</h2>
         </div>
     )
-}
-//Header.defaultProps={
- //   title: 'Indecision'
-//};
 
 export default Header;
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const Option = (props) => {
-    return(
+const Option = (props) => (
         <div>
         
         {props.optionText}
         <button onClick={(e)=>{props.handleDeleteOption(props.optionText)}}>Remove</button> 
         </div>
-    )
-};
+)
 
 export default Option;
